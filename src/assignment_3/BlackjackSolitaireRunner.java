e6b5184f9fe7155e80fdfc2c34310d90d7fe97a0
viktorxhzj <harryxh@live.com>
2020-02-04 14:26:55 +0800

    Initial commit

---
+++ src/assignment_3/BlackjackSolitaireRunner.java
@@ -0,0 +1,8 @@
+package assignment_3;
+
+public class BlackjackSolitaireRunner {
+    public static void main(String[] args) {
+        BlackjackSolitaire bjs = new BlackjackSolitaire();
+        bjs.play();
+    }
+}
