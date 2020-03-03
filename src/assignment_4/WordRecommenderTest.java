2fa76e2c09aad2fec8c5ce9870c5f7034e91780d
viktorxhzj <harryxh@live.com>
2020-03-03 16:23:58 +0800

    final version

--- src/assignment_4/WordRecommenderTest.java
+++ src/assignment_4/WordRecommenderTest.java
@@ -1,42 +1,32 @@
 package assignment_4;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WordRecommenderTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getSimilarityMetric() {
         WordRecommender recommender = new WordRecommender("engDictionary.txt");
-        recommender.getSimilarityMetric("olaf", "olaf");
+        double result1 = recommender.getSimilarityMetric("olaf", "oluf");
+        assertEquals(result1, 3);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getWordSuggestions() {
         WordRecommender recommender = new WordRecommender("engDictionary.txt");
-        String result = recommender.prettyPrint(recommender.getWordSuggestions("disslve", 3, 0.8, 11));
-        System.out.println(result);
+        ArrayList<String> result = recommender.getWordSuggestions("whut", 3, 0.5, 10);
+        System.out.println(recommender.prettyPrint(result));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getWordsWithCommonLetters() {
-        WordRecommender recommender = new WordRecommender("engDictionary.txt");
-        ArrayList<String> testcase = new ArrayList<>();
-        File userFile = new File("test.txt");
-        try {
-            Scanner inScan = new Scanner(userFile);
-            while (inScan.hasNext()) {
-                testcase.add(inScan.next());
-            }
-            inScan.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        ArrayList<String> result = recommender.getWordsWithCommonLetters("cardiovascular", testcase, 4);
-        System.out.println(recommender.prettyPrint(result));
+    }
+
+    @Test
+    void prettyPrint() {
     }
 }
